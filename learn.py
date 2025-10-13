d3366e9481ca5ab222a3f0cf7c75b828a86178ed
Jeevanthi Kashyap <50105615@lntfinsvcs.com>
2025-10-13 22:02:04 +0530

    last updated: monday-- repeated task prompt loop

--- learn.py
+++ learn.py
@@ -14,19 +14,11 @@ args = vars(parser.parse_args())
 app = args["app"]
 root_dir = args["root_dir"]
 
-
 print_with_color("Welcome to your app agent", "yellow")
-print_with_color("Press 1 to continue", "blue")
-user_input = ""
-while user_input != "1" and user_input != "2":
-    user_input = input()
 
 if not app:
     print_with_color("What is the name of the target app?", "blue")
     app = input()
     app = app.replace(" ", "")
 
-if user_input == "1":
-    os.system(f"python scripts/self_explorer.py --app {app} --root_dir {root_dir}")
-else:
-    print_with_color("Wrong key pressed","red")
+os.system(f"python scripts/self_explorer.py --app {app} --root_dir {root_dir}")
